ba7a185060225c2b3668de1ac47fc273c0799d26
KananValizada <kenan.veli@yahoo.com>
2021-12-04 19:56:37 +0400

    JWT login

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -11,17 +11,28 @@ import {
 import { GoogleLogin } from "react-google-login";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { signup, signin } from "../../actions/auth";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Input from "./Input";
 import useStyles from "./styles";
 import Icon from "./icon";
 
+const intitialState = {
+  firstname: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 export const Auth = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignUp] = useState(false);
+  const [formData, setFormData] = useState(intitialState);
   const classes = useStyles();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   // const isSignup = true;
 
   const handleShowPaswsord = () => {
@@ -29,8 +40,18 @@ export const Auth = () => {
       return !prevshowPassword;
     });
   };
-  const handleSubmit = () => {};
-  const handleChange = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSignup) {
+      dispatch(signup(formData, navigate));
+    } else {
+      dispatch(signin(formData, navigate));
+    }
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   const switchMode = () => {
     setIsSignUp((prevshowPassword) => {
       return !prevshowPassword;
